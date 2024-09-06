b5224c911f78b4ce7e7ce6d9d284abf959e22e48
Maciej Wojda <mwx27@protonmail.com>
2024-09-06 12:44:47 +0200

    added endpoint for checking balance

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('hello/', views.say_hello),
-  path('block_number/', views.get_current_block)
+  path('', views.say_hello),
+  path('block_number/', views.get_current_block),
+  path('balance', views.get_balance),
 ]
--- hello_world/views.py
+++ hello_world/views.py
@@ -11,7 +11,19 @@ else:
 
 def get_current_block(request):
     current_block_number = web3.eth.block_number
-    return HttpResponse(current_block_number)
+    return JsonResponse({"blockNumber": current_block_number})
+
+def get_balance(request):
+    address = request.GET.get('address')
+
+    if not Web3.is_address(address):
+        return JsonResponse({'error': 'Invalid address'}, status=400)
+
+    checksum_address = Web3.to_checksum_address(address)
+    balance_wei = web3.eth.get_balance(checksum_address)
+    balance_eth = web3.from_wei(balance_wei, 'ether')
+
+    return JsonResponse({"balance": balance_eth}, status=200)
 
 
 def say_hello(request):
